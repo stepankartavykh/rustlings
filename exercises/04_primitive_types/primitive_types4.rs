15e80fe92acd617aa33b20995186e2769e2adda0
sklion <sktvpx@gmail.com>
2024-02-04 11:14:17 +0300

    did some exercises

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -5,13 +5,18 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+use std::io;
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    println!("Start of the method.");
+    let mut index = String::new();
+    println!("index = {}", index);
+    
+
+    let nice_slice = &a[1..4];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
